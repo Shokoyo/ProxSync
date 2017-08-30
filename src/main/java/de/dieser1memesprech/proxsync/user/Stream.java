7474a0b45e725bb1af7abdbd42dafcb91d722a2c
Jeremias Lemm <jeremias.lemm@rwth-aachen.de>
2017-08-30 14:34:54 +0200

    can now parse proxer links. probably

---
+++ src/main/java/de/dieser1memesprech/proxsync/user/Stream.java
@@ -0,0 +1,22 @@
+package de.dieser1memesprech.proxsync.user;
+
+/**
+ * Created by Jeremias on 30.08.2017.
+ */
+public class Stream {
+    String id;
+    String code;
+    String type;
+    String htype;
+    String name;
+    String replace;
+    String img;
+    String parts;
+    String ssl;
+    String text;
+    String legal;
+    String uploader;
+    String username;
+    String tid;
+    String tname;
+}
